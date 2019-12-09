7f912091c5713dfe042a77ce6b965d2350da7cf7
Mhlengi Mzimela <mhlengi@digsconnect.com>
2019-12-09 11:28:13 +0200

    Added engineering puzzle solution

--- engineering_puzzle.py
+++ engineering_puzzle.py
@@ -1,18 +1,4 @@
-from itertools import combinations
-
-
-def find_subset_given_exactly_value_sum(data_list, value_sum):
-    subset_list = []
-    for value in range(len(data_list)):
-        subset_list.extend(
-            [
-                combination for combination in combinations(data_list, value)
-                if sum(combination) == value_sum
-            ]
-        )
-
-    return subset_list[0] if len(subset_list) > 0 else subset_list
-
+from functions import find_subset_given_exactly_value_sum
 
 list_of_areas_data_set = [
     18897109,
