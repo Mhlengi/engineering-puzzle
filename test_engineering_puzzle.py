542d7825c68d4d594b0098ce46991e58f5ca6abf
Mhlengi Mzimela <mhlengi@digsconnect.com>
2019-12-09 11:27:13 +0200

    Added basic unittest cases

--- test_engineering_puzzle.py
+++ test_engineering_puzzle.py
@@ -1,10 +1,35 @@
 import unittest
 
+from functions import find_subset_given_exactly_value_sum
 
 
-class LearningCase(unittest.TestCase):
-    def test_starting_out(self):
-        self.assertEqual(1, 1)
+class TestFindSubsetGivenExactlyValueSum(unittest.TestCase):
+    def test_find_subset_given_exactly_value_sum_with_valid_data_1_input(self):
+        local_list = [3, 5, 7, 10]
+        total_exactly = 15
+        subset = find_subset_given_exactly_value_sum(local_list, total_exactly)
+
+        expected_subset = (5, 10)
+
+        self.assertEqual(subset, expected_subset)
+
+    def test_find_subset_given_exactly_value_sum_with_valid_data_2_input(self):
+        local_list = [10, 7, 5, 3]
+        total_exactly = 15
+        subset = find_subset_given_exactly_value_sum(local_list, total_exactly)
+
+        expected_subset = (10, 5)
+
+        self.assertEqual(subset, expected_subset)
+
+    def test_find_subset_given_exactly_value_sum_with_invalid_data_input(self):
+        local_list = [10, 7, 5, 3]
+        total_exactly = 18
+        subset = find_subset_given_exactly_value_sum(local_list, total_exactly)
+
+        expected_subset = (10, 5)
+
+        self.assertNotEqual(subset, expected_subset)
 
 
 def main():
